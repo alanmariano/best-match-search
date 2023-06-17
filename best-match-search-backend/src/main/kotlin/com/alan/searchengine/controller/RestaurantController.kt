548abffb8cbbaa09d197908992de6bad67b1cb6a
Alan Mariano <alanmariano96@gmail.com>
2023-06-17 03:01:52 -0300

    feat: Using RequestParam in search endpoint to allow null search parameters

--- best-match-search-backend/src/main/kotlin/com/alan/searchengine/controller/RestaurantController.kt
+++ best-match-search-backend/src/main/kotlin/com/alan/searchengine/controller/RestaurantController.kt
@@ -3,10 +3,9 @@ package com.alan.searchengine.controller
 import com.alan.searchengine.dto.Restaurant
 import com.alan.searchengine.dto.RestaurantSearchParameters
 import com.alan.searchengine.service.RestaurantService
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,12 +17,12 @@ class RestaurantController (
     @GetMapping("/names")
     fun getRestaurantNames() : List<String> = restaurantService.getRestaurantNames()
 
-    @GetMapping("/{name}/{rating}/{distance}/{price}/{cuisine}")
-    fun getRestaurantMatches(@PathVariable name: String,
-                  @PathVariable rating: Double,
-                  @PathVariable distance: Double,
-                  @PathVariable price: Double,
-                  @PathVariable cuisine: String) : List<Restaurant> {
+    @GetMapping("/search")
+    fun getRestaurantMatches(@RequestParam name: String,
+                             @RequestParam(required = false) rating: Double?,
+                             @RequestParam(required = false) distance: Double?,
+                             @RequestParam(required = false) price: Double?,
+                             @RequestParam(required = false) cuisine: String?) : List<Restaurant> {
         return restaurantService.getRestaurantMatches(RestaurantSearchParameters(name, rating, distance, price, cuisine))
     }
 
